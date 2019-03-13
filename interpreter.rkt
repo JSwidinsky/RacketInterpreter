a953a327f352f208e7a54a9a5472dffd6135f5fe
JSwidinsky <swidinjo@gmail.com>
2019-03-12 19:58:54 -0600

    Added list processing function

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,53 +1,58 @@
 #lang racket
 
 (define (startEval prog)
-  (if (ArithmaticExpr? (car prog))
-      (ArithExpr prog)
-      (if (RelationalExpr? (car prog))
-          (RelationalExpr prog)
-          (if (equal? (car prog) `if)
-              (IfEval prog)
-              0))))
-      
-;function to evaluate an arithmatic expression
-(define (ArithExpr Expr)
-  (if (not (pair? Expr))
-      ;just return the number if it is an atom (i.e. it is just a number)
-      Expr
-      (if (equal? (car Expr) `+)
-          ;add the numbers together and return their sum
-          (+ (ArithExpr (car (cdr Expr))) (ArithExpr (car (cdr (cdr Expr)))))
-          (if (equal? (car Expr) `-)
-              ;subtract the numbers and return their difference
-              (- (ArithExpr (car (cdr Expr))) (ArithExpr (car (cdr (cdr Expr)))))
-              (if (equal? (car Expr) `*)
-                  ;multiply the numbers together and return their product
-                  (* (ArithExpr (car (cdr Expr))) (ArithExpr (car (cdr (cdr Expr)))))
-                  ;divide the numbers and return their quotient
-                  (/ (ArithExpr (car (cdr Expr))) (ArithExpr (car (cdr (cdr Expr))))))))))
+  (if (not (pair? prog))
+      prog
+      (if (equal? (car prog) `quote)
+          (quote (cdr prog))
+          (if (arithmaticExpr? (car prog))
+              (arithmaticExpr prog)
+              (if (relationalExpr? (car prog))
+                  (relationalExpr prog)
+                  (if (listOperator? (car prog))
+                      (listOperation prog)
+                      (if (equal? (car prog) `if)
+                          (ifEval prog)
+                          0)))))))
 
 
-;function to evaluate a relational expression
-(define (RelationalExpr Expr)
-  (if (equal? (car Expr) `=)
-      (= (ArithExpr (car (cdr Expr))) (ArithExpr (car (cdr (cdr Expr)))))
-      (if (equal? (car Expr) `<=)
-          (<= (ArithExpr (car (cdr Expr))) (ArithExpr (car (cdr (cdr Expr)))))
-          (if (equal? (car Expr) `<)
-              (< (ArithExpr (car (cdr Expr))) (ArithExpr (car (cdr (cdr Expr)))))
-              (if (equal? (car Expr) `>=)
-                  (>= (ArithExpr (car (cdr Expr))) (ArithExpr (car (cdr (cdr Expr)))))
-                  (> (ArithExpr (car (cdr Expr))) (ArithExpr (car (cdr (cdr Expr))))))))))
+(define (arithmaticExpr Expr)
+  (case (car Expr)
+    (`+ (+ (startEval (car (cdr Expr))) (startEval (car (cdr (cdr Expr))))))
+    (`- (- (startEval (car (cdr Expr))) (startEval (car (cdr (cdr Expr))))))
+    (`* (* (startEval (car (cdr Expr))) (startEval (car (cdr (cdr Expr))))))
+    (`/ (/ (startEval (car (cdr Expr))) (startEval (car (cdr (cdr Expr))))))
+    ))
 
-(define (IfEval Expr)
-  (if (RelationalExpr (car (cdr Expr)))
+(define (relationalExpr Expr)
+  (case (car Expr)
+    (`equal? (equal? (car (cdr Expr)) (car (cdr (cdr Expr)))))
+    (`= (= (startEval (car (cdr Expr))) (startEval (car (cdr (cdr Expr))))))
+    (`<= (<= (startEval (car (cdr Expr))) (startEval (car (cdr (cdr Expr))))))
+    (`< (< (startEval (car (cdr Expr))) (startEval (car (cdr (cdr Expr))))))
+    (`>= (>= (startEval (car (cdr Expr))) (startEval (car (cdr (cdr Expr))))))
+    (`> (> (startEval (car (cdr Expr))) (startEval (car (cdr (cdr Expr))))))
+    ))
+
+;this might not work yet...
+(define (listOperation Expr)
+  (case (car Expr)
+    (`car (car (cdr Expr)))
+    (`cdr (cdr (cdr Expr)))
+    (`cons (cons (car (cdr Expr)) (car (cdr (cdr Expr)))))
+    (`pair? (pair? Expr))
+    ))
+
+(define (ifEval Expr)
+  (if (startEval (car (cdr Expr)))
       (startEval (car (cdr (cdr Expr))))
       (startEval (car (cdr (cdr (cdr Expr)))))))
 
-
-
-(define (ArithmaticExpr? expr)
+(define (arithmaticExpr? expr)
   (or (or (or (equal? expr `+) (equal? expr `-)) (equal? expr `*)) (equal? expr `/)))
 
-(define (RelationalExpr? expr)
-  (or (or (or (or (equal? expr `=) (equal? expr `<=)) (equal? expr `<)) (equal? expr `>=)) (equal? expr `>)))
+(define (relationalExpr? expr)
+  (or (or (or (or (or (equal? expr `=) (equal? expr `<=)) (equal? expr `<)) (equal? expr `>=)) (equal? expr `>)) (equal? expr `equal?)))
+
+(define (listOperator? expr)
+  (or (or (or (equal? expr `cons) (equal? expr `car)) (equal? expr `cdr)) (equal? expr `pair?)))
